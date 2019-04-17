762d2b5b62b14be69a7ab597d7b99e87537804de
John Levin <jlevin@tlmworks.org>
2019-04-17 09:37:58 -0700

    Insert user now hashes password.  Added routes for getting all users, or a single user by id or username

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require('express')
 const mysql = require('mysql')
-
-const router = express.Router();
+const bcrypt = require('bcrypt-nodejs')
 const dbconfig = require('../config/database')
 
+const router = express.Router();
 const connection = mysql.createConnection(dbconfig.connection)
 
 // establish connection to mysql database
@@ -13,11 +13,55 @@ connection.query(`USE ${dbconfig.database}`)
 // GET -  retrieve a list of all users
 //
 router.get('/', (req, res) => {
-  res.json({});
-});
+  connection.query(
+    'CALL selectUser(?, ?)',
+    [null, null],
+    (err, responseObject) => {
+      // if sql returns an error,  forward to client
+      if (err) {
+        return res.json({
+          success: false,
+          message: err.sqlMessage,
+        })
+      }
+      // otherwise, extract relevant info from responseObject and send to client
+      const returnedData = responseObject[0]
+      return res.json({
+        success: true,
+        results: returnedData,
+      })
+    },
+  )
+})
 
 //
-// POST - insert a new user into users table
+// GET -  retrieve a single user by id or username
+//
+router.get('/:user', (req, res) => {
+  const isNumeric = /^\d+$/.test(req.params.user)
+  connection.query(
+    'CALL selectUser(?, ?)',
+    isNumeric ? [req.params.user, null] : [null, req.params.user],
+    (err, responseObject) => {
+      // if sql returns an error,  forward to client
+      if (err) {
+        return res.json({
+          success: false,
+          message: err.sqlMessage,
+        })
+      }
+      // otherwise, extract relevant info from responseObject and send to client
+      const returnedData = responseObject[0]
+      return res.json({
+        success: true,
+        results: returnedData,
+      })
+    },
+  )
+})
+
+//
+// POST - insert a new user
 //
 router.post('/', (req, res) => {
   // destructure required form fields from req.body
@@ -30,10 +74,11 @@ router.post('/', (req, res) => {
     comments,
     roleId,
   } = req.body
+  const hashedPassword = bcrypt.hashSync(password, null, null)
   // call mysql stored procedure to insert new user
   connection.query(
     'CALL insertUser(?, ?, ?, ?, ?, ?, ?)',
-    [username, firstName, lastName, password, email, comments, roleId],
+    [username, firstName, lastName, hashedPassword, email, comments, roleId],
     (err, responseObject) => {
       // if sql returns an error, forward to client
       if (err) {
@@ -46,9 +91,48 @@ router.post('/', (req, res) => {
       const returnedData = responseObject[0]
       const result = returnedData[0]
       return res.json({
-        success: true,
+        success: !!result.success,
         message: result.msg,
-        user_id: result.userId,
+        userId: result.userId,
+      })
+    },
+  )
+})
+
+//
+// PUT - update a user
+//
+router.put('/', (req, res) => {
+  // destructure required form fields from req.body
+  const {
+    userId,
+    username,
+    firstName,
+    lastName,
+    password,
+    email,
+    comments,
+    roleId,
+  } = req.body
+  const hashedPassword = bcrypt.hashSync(password, null, null)
+  // call mysql stored procedure to insert new user
+  connection.query(
+    'CALL updateUser(?, ?, ?, ?, ?, ?, ?, ?)',
+    [userId, username, firstName, lastName, hashedPassword, email, comments, roleId],
+    (err, responseObject) => {
+      // if sql returns an error, forward to client
+      if (err) {
+        return res.json({
+          success: false,
+          message: err.sqlMessage,
+        })
+      }
+      // otherwise, extract relevant info from responseObject and send to client
+      const returnedData = responseObject[0]
+      const result = returnedData[0]
+      return res.json({
+        success: !!result.success,
+        message: result.msg,
       })
     },
   )
