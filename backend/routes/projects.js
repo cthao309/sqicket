4fc093109935fbd8399d6705a6e837fd18f743d5
hhemphill@sq.code7370.org <hhemphill@tlmworks.org>
2019-04-17 11:37:12 -0700

    Can create, delete, and get all projects collectively and individually by name and projectId

--- backend/routes/projects.js
+++ backend/routes/projects.js
@@ -14,9 +14,52 @@ connection.query(`USE ${dbconfig.database}`)
 // GET -  retrieve a list of all projects
 //
 router.get('/', (req, res) => {
-  res.json({});
+  connection.query(
+    'CALL getProject(?, ?)',
+    [null, null],
+    (err, responseObject) => {
+      // if sql returns an error,  forward to client
+      if (err) {
+        return res.json({
+          success: false,
+          message: err.sqlMessage,
+        })
+      }
+      // otherwise, extract relevant info from responseObject and send to client
+      const returnedData = responseObject[0]
+      return res.json({
+        success: true,
+        results: returnedData,
+      })
+    },
+  )
 });
 
+//
+// GET -  retrieve a single project by id or projectname
+//
+router.get('/:project', (req, res) => {
+  const isNumeric = /^\d+$/.test(req.params.project)
+  connection.query(
+    'CALL getProject(?, ?)', 
+    isNumeric ? [req.params.project, null] : [null, req.params.project],
+    (err, responseObject) => {
+      // if sql returns an error,  forward to client
+      if(err) {
+        return res.json({
+          success:false,
+          message: err.sqlMessage,
+        })
+      }
+      // otherwise, extract relevant info from responseObject and send to client
+      const returnedData = responseObject[0]
+      return res.json({
+        success: true,
+        results: returnedData,
+      })
+    }
+  ) 
+})
 //
 // POST - insert a new project into projects table
 //
@@ -27,7 +70,7 @@ router.post('/', (req, res) => {
     projectDescription,
     createdByUserId,
   } = req.body
-  console.log(projectName,projectDescription,createdByUserId)
+  console.log(projectName,projectDescription,createdByUserId, 'req.body')
   // call mysql stored procedure to insert new project
   connection.query(
     'CALL insertProject(?, ?, ?)',
@@ -44,11 +87,43 @@ router.post('/', (req, res) => {
       const returnedData = responseObject[0]
       const result = returnedData[0]
       return res.json({
-        success: true,
+        success: !!result.success,
         message: result.msg,
       })
     },
   )
 })
+
+//
+// DELETE - mark a project is being deleted
+//
+router.delete('/', (req, res) => {
+  // de-structure projectId from req.body
+  const { projectId } = req.body
+  // console.log(req.body, 'req.body')
+  // call mysql stored procedure to delete user by projectId
+  connection.query(
+    'CALL deleteProject(?)',
+    [projectId],
+    (err, responseObject) => {
+      // if sql returns an error,  forward to client
+      if (err) {
+        return res.json({
+          success: false,
+          message: err.sqlMessage,
+        })
+      }
+      // otherwise, extract relevant info from responseObject and send to client
+      const returnedData = responseObject[0]
+      const result = returnedData[0]
+      return res.json({
+        success: !!result.success,
+        message: result.msg,
+      })
+    },
+  )
+})
+
+
 
 module.exports = router;
