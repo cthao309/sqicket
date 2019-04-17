3d5b9e3d100fb53b163869eda027251d007b41ef
John Levin <jlevin@tlmworks.org>
2019-04-17 14:32:15 -0700

    Added routes for assignments by userId and assignments by projectId

--- backend/routes/assignments.js
+++ backend/routes/assignments.js
@@ -7,7 +7,7 @@ const router = express.Router();
 // GET -  retrieve a list of all assignments
 //
 router.get('/', (req, res) => {
-  sp.getAssignments(
+  sp.getAssignment(
     null,
     null,
     (err, responseObject) => {
@@ -28,4 +28,53 @@ router.get('/', (req, res) => {
   )
 })
 
+//
+// GET -  retrieve assignments by userId
+//
+router.get('/user/:userId', (req, res) => {
+  sp.getAssignment(
+    req.params.userId,
+    null,
+    (err, responseObject) => {
+      // if sql returns an error,  forward to client
+      if (err) {
+        return res.json({
+          success: false,
+          message: err.sqlMessage,
+        })
+      }
+      // otherwise, extract relevant info from responseObject and send to client
+      const returnedData = responseObject[0]
+      return res.json({
+        success: true,
+        results: returnedData,
+      })
+    },
+  )
+})
+
+//
+// GET -  retrieve assignments by projectId
+//
+router.get('/project/:projectId', (req, res) => {
+  sp.getAssignment(
+    null,
+    req.params.projectId,
+    (err, responseObject) => {
+      // if sql returns an error,  forward to client
+      if (err) {
+        return res.json({
+          success: false,
+          message: err.sqlMessage,
+        })
+      }
+      // otherwise, extract relevant info from responseObject and send to client
+      const returnedData = responseObject[0]
+      return res.json({
+        success: true,
+        results: returnedData,
+      })
+    },
+  )
+})
 module.exports = router;
