6815f642cf87d7bedd0727c57cd9e8469db8213d
John Levin <jlevin@tlmworks.org>
2019-04-17 07:52:56 -0700

    Added user routes for insert and delete

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -9,12 +9,16 @@ const connection = mysql.createConnection(dbconfig.connection)
 // establish connection to mysql database
 connection.query(`USE ${dbconfig.database}`)
 
-/* GET users listing. */
+//
+// GET -  retrieve a list of all users
+//
 router.get('/', (req, res) => {
   res.json({});
 });
 
-// POST insert new user into users table
+//
+// POST - insert a new user into users table
+//
 router.post('/', (req, res) => {
   // destructure required form fields from req.body
   const {
@@ -31,7 +35,7 @@ router.post('/', (req, res) => {
     'CALL insertUser(?, ?, ?, ?, ?, ?, ?)',
     [username, firstName, lastName, password, email, comments, roleId],
     (err, responseObject) => {
-      // if sql returns an error, send to client as JSON
+      // if sql returns an error, forward to client
       if (err) {
         return res.json({
           success: false,
@@ -40,11 +44,40 @@ router.post('/', (req, res) => {
       }
       // otherwise, extract relevant info from responseObject and send to client
       const returnedData = responseObject[0]
-      const user = returnedData[0]
+      const result = returnedData[0]
       return res.json({
         success: true,
-        message: user.msg,
-        user_id: user.userId,
+        message: result.msg,
+        user_id: result.userId,
+      })
+    },
+  )
+})
+
+//
+// DELETE - mark a user a being deleted
+//
+router.delete('/', (req, res) => {
+  // destructure userId from req.body
+  const { userId } = req.body
+  // call mysql stored procedure to delete user by userId
+  connection.query(
+    'CALL deleteUser(?)',
+    [userId],
+    (err, responseObject) => {
+      // if sql returns an error,  forward to client
+      if (err) {
+        return res.json({
+          success: false,
+          message: err.sqlMessage,
+        })
+      }
+      // otherwise, extract relevant info from responseObject and send to client
+      const returnedData = responseObject[0]
+      const result = returnedData[0]
+      return res.json({
+        success: !!result.success,
+        message: result.msg,
       })
     },
   )
